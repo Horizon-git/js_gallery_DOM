414f6baf49b43e09d16ead27a0bb07b63d976b0b
Horizon-git <apbreloaded947@gmail.com>
2024-02-21 23:27:23 +0200

    make build

---
+++ index.beeaa4ef.js
@@ -0,0 +1,3 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var n=t.target.closest(".list-item__link");n&&(e.src=n.href)});//# sourceMappingURL=index.beeaa4ef.js.map
+
+//# sourceMappingURL=index.beeaa4ef.js.map
